e2e09486b7097c88b91885fe4add8f3aa68d65cb
VYRUZG <140122918+VYRUZG@users.noreply.github.com>
2024-06-20 11:38:14 -0600

    navbar

---
+++ assets/navbar/navbar.php
@@ -0,0 +1,24 @@
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+    nav{
+        background-color: #A61B0F;
+        }
+</style>
+<nav class="navbar navbar-expand-lg navbar-dark nav"> 
+    <div class="container">
+    <a class="navbar-brand" href="#" data-abc="true">
+        <img src="../img/UPTx-blanco.png" alt="upt" style="width: 15%; height: auto;">
+    </a> 
+    </div>
+    <div class="ml-auto col-4"> 
+            <form onsubmit="event.preventDefault()" class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+</nav>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
